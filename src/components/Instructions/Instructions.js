e75fdf41d104ff18a2a2eb6921328a04f5b65f67
TanyaOdegova <odegovat@yahoo.com>
2019-06-17 16:31:47 -0400

    updated style for all componenets

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,7 +4,6 @@ import "./Instructions.css";
 const  Instructions = props => (
     <div className="col-md-12">
         <div className="Instructions">
-            <h1>Clicky Game!</h1>
             <h2>Click on an image to earn points, but don't click on any more than once!</h2>
         </div>
     </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Nav.css';
 
-const Nav = props => (
-    <nav className="navbar navbar-default" >
-        <ul>
-            <li className="brand">Clicky Game</li>
-            <li>Click on the image to begin!</li>
-            <li>Score: {props.score} | Top Score: {props.topScore}  </li>
-        </ul>
-    </nav>
-);
-
-
-export default Nav;
+    const Nav = props => (
+        <nav className="navbar navbar-default" >
+            <ul>
+                <li className="brand">Clicky Game</li>
+                <li>Current Score: {props.score} | Top Score: {props.highScore}  </li>
+            </ul>
+        </nav>
+    );
+    
+    
+    export default Nav;
